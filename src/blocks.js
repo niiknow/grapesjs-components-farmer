ddff38c4054ad07d43e440e7e9b60e013f463bcc
noogen <tom@noogen.net>
2019-02-25 16:14:49 -0600

    init

--- src/blocks.js
+++ src/blocks.js
@@ -9,7 +9,7 @@ export default (editor, opt = {}) => {
     category: opt.categoryLabel,
   }
 
-  bm.add('mj-1-column', {
+  bm.add('form-1-column', {
     label: '1 Column',
     content: `<mj-section>
         <mj-column><mj-text>Content 1</mj-text></mj-column>
@@ -18,7 +18,7 @@ export default (editor, opt = {}) => {
     ...allBlocks,
   });
 
-  bm.add('mj-2-columns', {
+  bm.add('form-2-columns', {
     label: '2 Columns',
     content: `<mj-section>
         <mj-column><mj-text>Content 1</mj-text></mj-column>
@@ -28,7 +28,7 @@ export default (editor, opt = {}) => {
     ...allBlocks,
   });
 
-  bm.add('mj-3-columns', {
+  bm.add('form-3-columns', {
     label: '3 Columns',
     content: `<mj-section>
         <mj-column><mj-text>Content 1</mj-text></mj-column>
@@ -39,45 +39,24 @@ export default (editor, opt = {}) => {
     ...allBlocks,
   });
 
-  bm.add('mj-text', {
+  bm.add('form-text', {
     label: 'Text',
     content: '<mj-text>Insert text here</mj-text>',
     attributes: { class: 'gjs-fonts gjs-f-text' },
     ...allBlocks,
   });
 
-  bm.add('mj-button', {
+  bm.add('form-button', {
     label: 'Button',
     content: '<mj-button>Button</mj-button>',
     attributes: { class: 'gjs-fonts gjs-f-button' },
     ...allBlocks,
   });
 
-  bm.add('mj-image', {
+  bm.add('form-image', {
     label: 'Image',
-    content: '<mj-image src="http://placehold.it/350x250/78c5d6/fff">',
+    content: '<mj-image src="https://placehold.it/350x250/78c5d6/fff">',
     attributes: { class: 'fa fa-image' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-divider', {
-    label: 'Divider',
-    content: '<mj-divider/>',
-    attributes: { class: 'gjs-fonts gjs-f-divider'},
-    ...allBlocks,
-  });
-
-  bm.add('mj-social', {
-    label: 'Social',
-    content: '<mj-social/>',
-    attributes: { class: 'fa fa-share-alt' },
-    ...allBlocks,
-  });
-
-  bm.add('mj-spacer', {
-    label: 'Spacer',
-    content: '<mj-spacer/>',
-    attributes: { class: 'fa fa-arrows-v' },
     ...allBlocks,
   });
 }
--- src/index.js
+++ src/index.js
@@ -3,48 +3,6 @@ export default (editor, opts = {}) => {
 
     categoryLabel: '',
 
-    // Code viewer theme
-    codeViewerTheme: 'hopscotch',
-
-    // Import placeholder MJML
-    importPlaceholder: '',
-
-    // Title for the import modal
-    modalTitleImport: 'Import MJML',
-
-    // Test for the import button
-    modalBtnImport: 'Import',
-
-    // Description for the import modal
-    modalLabelImport: '',
-
-    // Title for the export modal
-    modalTitleExport: 'Export MJML',
-
-    // Description for the export modal
-    modalLabelExport: '',
-
-    // Overwrite default export command
-    overwriteExport: 1,
-
-    // String before the MJML in export code
-    preMjml: '',
-
-    // String after the MJML in export code
-    postMjml: '',
-
-    // Export 'mjml', 'html' or both (leave empty) TODO
-    exportOnly: '',
-
-    // Clean all previous blocks if true
-    resetBlocks: 1,
-
-    // Clean all previous devices and set a new one for mobile
-    resetDevices: 1,
-
-    // Reset the Style Manager and add new properties for MJML
-    resetStyleManager: 1,
-
     // Column padding (this way it's easier select columns)
     columnsPadding: '10px 0',
 
@@ -66,21 +24,6 @@ export default (editor, opts = {}) => {
   // Add Components
   require('./components').default(editor, opt);
 
-  // Add Commands
-  require('./commands').default(editor, opt);
-
   // Add Buttons
   require('./buttons').default(editor, opt);
-
-  // Extend Style Manager
-  require('./style').default(editor, opt);
-
-  // Update devices
-  if (opt.resetDevices) {
-    const dm = editor.DeviceManager;
-    dm.getAll().reset();
-    dm.add('Desktop', '');
-    dm.add('Mobile', '320px');
-  }
-
 };
